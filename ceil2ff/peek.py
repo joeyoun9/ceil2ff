762f30355a02117c34484033e0016cbf53651700
Joe Young <joeyoungwx@gmail.com>
2012-01-09 08:59:30 -0700

    Added a new 'peek' method

--- ceil2ff/peek.py
+++ ceil2ff/peek.py
@@ -42,3 +42,48 @@ def peek(fd,time=False):
 	plt.colorbar(ceil).set_label('Attenuated Backscatter $m^{-1}sr^{-1}$')
 	plt.show()
 
+def ShowLow(fd='./ceil.dat'):
+
+	from cleanfig import *
+	f = open(fd)
+	vals = []
+	times = []
+	heights = []
+	#if not time:
+	#	time = int(f.readline().split(',')[0])
+	f.seek(0)
+	t = 0
+	for ob in f.readlines():
+		# loop through each line in the file... inefficient, but necessary I think
+		od = ob.split(',')
+		t = int(od[0])
+		#if t < time or t > time + 1800:
+		#	continue
+		#	# can't break, since the data may not be in order!
+		#else:
+		# save the profile to the list of values!
+		vals.append(od[3:])
+		times.append(t)
+		heights = [x * int(od[1]) for x in range(len(od[3:]))] # not append!
+	print "Plotting"
+	# and now plot
+
+	#fig = figure()
+	fig = plt.figure()
+	#ax = fig.gca()
+	ax,ax2 = init_axis(1,1,1,twin=True)
+	ttUTC(ax,'x',times[0],times[-1])
+	ttMST(ax2,'x',times[0],times[-1])
+
+	ax.set_xlabel("Time (UTC)")
+	ax2.set_xlabel("Time (MST)")
+
+	#ceil = plt.imshow(flip2d(vals),origin='lower',aspect=len(times)/len(heights))
+	#ceil = plt.pcolormesh(array(times),array(heights),flip2d(vals),levels=[.01*x for x in range(400)])
+	ceil= plt.contourf(times,heights,flip2d(vals),levels=[-.1+.01*x for x in range(50)])
+	plt.colorbar(ceil).set_label('Attenuated Backscatter $m^{-1}sr^{-1}$')
+	fig_size(fig,15,8)
+	ax.set_ylabel("Range (m)")
+	print "saving"
+	#plt.savefig('7Jan_full.png')
+	plt.show()
