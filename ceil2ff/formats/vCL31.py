b42840efe04fe30ea3d6c61ea7a20c24e83fd744
Joe Young <joeyoungwx@gmail.com>
2012-12-19 10:27:40 -0600

    rewrote CL31 message processor from Thesis work

--- ceil2ff/formats/vCL31.py
+++ ceil2ff/formats/vCL31.py
@@ -1,46 +1,76 @@
 """
-	Read CL31 backscatter data
+Read and produce output from a CL31 observation
+
 """
-from numpy import int,zeros,float32
+import numpy as np
 
-def read(ob,**kwargs):
-	"""
-		Read a cl31 message 1 or two observation
-	"""
+def read(ob):
+	'''
+	Read and translate a single Vaisala CL31 message 2(?) observation text
+	as reported directly from the ceilometer.
+	
+	Parameters
+	----------
+	ob: str
+		the text that is returned by the ceilometer between the begin comms 
+		and end comms control characters (unichr(001 - 004))
+	
+	Returns
+	-------
+	dict : 'bs', 'height', 'status'
+		bs: the range corrected un-scaled (assuming a scale of 1e9) log10'd bs
+			coefficient
+		'height': range indices for the non-time dimension of the output 
+			corresponding to the vertical data key
+		'status': 13 transcribed floating point numbers which give cloud height
+			information, and status information. It is possible to get more
+			information from these lines with a revision of this code.
+	'''
+	OB_LENGTH = 770 #FIXME - the current return length is limited to 770
 	SCALING_FACTOR = 1.0e9
-	#if not scaled: # no! it is always scaled!
-	#	SCALING_FACTOR = 1
-	# this is a data set, ob is a array of lines
-	tm = int(ob['time'])
-	data = ob['rest'].split("\n") # split into lines
+	# break the full ob text into it's constituent parts
+	p1 = ob.split(unichr(002))
+	p2 = p1[1].split(unichr(003))
+	code = p1[0].strip()
+	ob = p2[0].strip() # just contents between B and C
+	checksum = p2[1].strip()
 
-	#UPDATE 5/Jan/2012 - changed data line to -1 because of .strip on the output
-	prof = data[-2].strip() # important to obliterate that little guy...
+	data = ob.split("\n") # split into lines
 
-	code = ob['code']
-	# this is the first little bit of info at the top, very informative
+	'the last line of the profile should be the data line'
+	prof = data[-1].strip()
+	'grab status lines'
+	sl1 = data[0].strip()
+	sl2 = data[-2].strip() # I will skip any intermediate data lines...
 
-	# then just grab the extra lines - since this compress tries to save everything
-	# FIXME cloud data does not currently get processed!!!
-	cld = 'CL31'+ob['code'].strip()+data[1].strip()+data[2].strip() +"|"
-
+	status = np.array([sl1[0].replace('/','0'),
+					sl1[1].replace('A','2').replace('W','1')]+
+					sl1[2:-13].replace('/','0').split() + sl2[:-14].split(),
+					dtype=np.float32)
+	'status should have a length of 13... we shall see...'
 	# determine height difference by reading the last digit of the code
 	height_codes = [0,10,20,5,5] #'0' is not a valid key, and will not happen
 	data_lengths = [0,770,385,1500,770]
-	# length betwwn 770 and 1500
-	datLen = data_lengths[int(code[-1])] # figure out the expcted message length
-	htMult = height_codes[int(code[-1])] # assumes that newlines and spaces have been strip()ed off
-	values = zeros(datLen,dtype=float32)
+	'length between 770 and 1500'
+	datLen = data_lengths[int(code[-1])]
+	htMult = height_codes[int(code[-1])]
+	values = np.zeros(datLen,dtype=np.float32)
 	ky = 0
 	for i in xrange(0,len(prof),5):
 		
 		ven = prof[i:i+5]
 		if ven[0:2] == "ff" or ven == '00000':
 			# logic: ff corresponds to >=ff000, which is ~1e6, which is beyond super high
-			values[ky] = 1/SCALING_FACTOR
+			values[ky] = 1
 		else:
-			values[ky] = int(ven,16) / SCALING_FACTOR # scaled to 100000sr/km (x1e9 sr/m)FYI
+			values[ky] = int(ven,16) # scaled to 100000sr/km (x1e9 sr/m)FYI
 		ky += 1 # keep the key up to date
-	out = {'t':tm,'h':htMult,'v':values,'c':cld,'l':datLen}
+
+	# then the storage will be log10'd values
+	out = {
+		'height':np.arange(0,10000,htMult)[:OB_LENGTH],
+		'bs':np.log10(values[:OB_LENGTH]/SCALING_FACTOR),
+		'status':status
+		}
 	return out
 
